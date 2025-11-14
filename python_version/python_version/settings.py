d9a546012df9ea13a47a366ac21a009148e42eed
Rashandd <89455752+Rashandd@users.noreply.github.com>
2025-11-15 00:09:05 +0300

    Update settings.py

--- python_version/python_version/settings.py
+++ python_version/python_version/settings.py
@@ -128,7 +128,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Bütün statik dosyaların toplanacağı yer
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
